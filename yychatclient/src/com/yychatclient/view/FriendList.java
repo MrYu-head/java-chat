37d51ce104b3695297be3bb27fc9de7f5da1d086
yuqianlei <2537011422@qq.com>
2019-04-28 00:05:21 +0800

    提交文件

--- yychatclient/src/com/yychatclient/view/FriendList.java
+++ yychatclient/src/com/yychatclient/view/FriendList.java
@@ -169,9 +169,9 @@ public class FriendList extends JFrame implements ActionListener,MouseListener{/
 		//
 		String[] friendName=friendString.split(" ");
 		int count=friendName.length;
-		for(int i=0;i<count;i++){
-			myFriendJLabel[Integer.parseInt(friendName[i])].setEnabled(true);
-		}
+//		for(int i=0;i<count;i++){
+//			myFriendJLabel[Integer.parseInt(friendName[i])].setEnabled(true);
+//		}
 	}
 	@Override
 	public void actionPerformed(ActionEvent argo){
--- yychatserver/src/com/yychatserver/controller/StartServer.java
+++ yychatserver/src/com/yychatserver/controller/StartServer.java
@@ -5,8 +5,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
+
+import com.mysql.jdbc.PreparedStatement;
 import com.yychat.model.Message;
 import com.yychat.model.User;
 
@@ -36,6 +42,43 @@ public class StartServer {
 		System.out.println(userName);
 		System.out.println(passWord);
 		
+		//从数据库中实现用户的登录验证
+		//1、加载驱动程序
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("已经加载了数据库驱动！");
+	
+		//2、连接数据库
+		String url="jdbc:mysql://127.0.0.1:3306/yychat";
+		//中文用户名必须用下面的url
+		//String url="jdbc:mysql://127.0.0.1:3306/yychat?useUnicode=true&characterEncoding=UTF-8";
+		String dbUser="root";
+		String dbPass="";				
+		Connection conn=DriverManager.getConnection(url,dbUser,dbPass);
+		
+		//3、创建PreparedStatement对象，用来执行SQL语句
+		String user_Login_Sql="select * from user where username=? and password=?";
+		java.sql.PreparedStatement ptmt=conn.prepareStatement(user_Login_Sql);
+		ptmt.setString(1, userName);
+		ptmt.setString(2, passWord);
+		
+		//4、执行查询，返回结果集
+		ResultSet rs=ptmt.executeQuery();
+		
+		//5、根据结果集来判断是否能登录
+		boolean loginSuccess=rs.next();	
+//		//2、获取连接对象
+//		String url="";
+//		String dbuser="root";
+//		String dbpass="";
+//		Connection conn=DriverManager.getConnection(url,dbuser,dbpass);
+//		
+//		
+		
+		
+		
+		
+		
+		
 		//
 		Message mess=new Message();
 		mess.setSender("Server");
@@ -61,8 +104,17 @@ public class StartServer {
 			
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+	
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
